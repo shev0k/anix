daa59dca773741d17e1c3dcc6b67fe907838a5be
Claudiu <499309@student.fontys.nl>
2023-10-31 22:40:29 +0100

    - added user dashboard

---
+++ AniX/AniX/Pages/ProfileEdit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AniX_WEB.Pages
+{
+    public class ProfileEditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
